a599dd606e6976c1284eddef23f6b23dc4cf5302
Guilherme Souza <gui.edu0380@gmail.com>
2024-01-27 15:59:25 -0300

    feat(index.ts) adicionando codigos de estudo utilizando expressões regulares

---
+++ expressoes-regulares/index.ts
@@ -0,0 +1,22 @@
+function phoneValidator(phoneNumber: String) {
+    const fixedString = phoneNumber.replace(/[\sa-zA-Z]/g, '');
+
+    const countryCode = fixedString.match(/(?<=\+)\d{1,3}/)
+    if (countryCode) {
+        console.log(countryCode[0]);
+    }
+
+    const ddd = fixedString.match(/(?<=\()\d+(?=\))/)
+    if (ddd) {
+        console.log(ddd[0])
+    }
+
+    const number = fixedString.match(/(?<=\)).+()/)
+    if (number) {
+        console.log(number[0])
+    }
+}
+
+phoneValidator('+55 (12) 9 9111 - 1111 abcdef')
+phoneValidator('+1 (12) 9 9111 - 1111 abcdef')
+phoneValidator('+143 (43) 555-333-1234')
