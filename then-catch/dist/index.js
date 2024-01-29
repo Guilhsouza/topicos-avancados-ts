41c69aa56419beb29e359dce7044ea35f822ef34
Guilherme Souza <gui.edu0380@gmail.com>
2024-01-29 19:20:29 -0300

    feat(then-catch) adicionando estudos sobre then e catch

---
+++ then-catch/dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+function execute() {
+    return new Promise((resolve) => {
+        console.log('promise executando...');
+        setTimeout(() => {
+            console.log('resolvendo promise');
+            resolve('resultado');
+        }, 2000);
+    });
+}
+execute().then((result) => {
+    console.log(result);
+}).catch((err) => {
+    console.log(err);
+});
